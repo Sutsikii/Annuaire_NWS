dc127a18c5a65447e4384a225a71106fcf297dc7
Sutsikii <nicolas-dutertre@bbox.fr>
2022-10-14 09:38:03 +0200

    CSS files

--- index.php
+++ index.php
@@ -5,11 +5,28 @@ require_once 'Class/Form.php';
 
 $form = new Form($_POST);
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- <link rel="stylesheet" href="public/css/reset.css"> -->
+    <link rel="stylesheet" href="public/css/style.css">
+    <title>Annuaire NWS</title>
+</head>
+<body>
 
-<form action="#" method="post">
+    <h1>Annuaire Normandie Web School</h1>
+
     <?php
     echo $form->input('username');
     echo $form->input('password');
     echo $form->submit();
     ?>
+
+</body>
+</html>
+<form action="#" method="post">
+
 </form>
