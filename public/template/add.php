9f930148419aa74c006ef284a47ae710a9eefe6f
Sutsikii <nicolas-dutertre@bbox.fr>
2022-10-14 21:17:26 +0200

    Supp Commentaire Reset Css

--- public/template/add.php
+++ public/template/add.php
@@ -10,7 +10,7 @@ $form = new Form($_POST);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- <link rel="stylesheet" href="public/css/reset.css"> -->
+    <link rel="stylesheet" href="public/css/reset.css">
     <link rel="stylesheet" href="../css/style.css">
     <title>Annuaire NWS</title>
 </head>
