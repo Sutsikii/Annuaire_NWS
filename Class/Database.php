145708d679dd4324c0e130eb4a761304212af83f
nicolas <nicolas-dutertre@bbox.fr>
2022-10-28 14:02:58 +0200

    Json fonc

--- Class/Database.php
+++ Class/Database.php
@@ -1,16 +1,24 @@
 <?php
 
+
+
 class Database
 {
 
     protected $pdo;
 
+    public function getConfig()
+    {
+        echo $bddData;
+    }
 
     protected function getPDO()
     {
         if($this->pdo === null)
         {
-            $pdo = new PDO("mysql:host=localhost;dbname=annuaire", "root", "");
+            $json_data = file_get_contents("../../database.config.json");
+            $data_bdd = json_decode($json_data);
+            $pdo = new PDO('mysql:host='.$data_bdd->server.';dbname='.$data_bdd->database, $data_bdd->user, $data_bdd->password);
             $this->pdo = $pdo;
             return $pdo;
         }
